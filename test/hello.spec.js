5d259a4c46fb0a84c50e8abe07d12a8816c3f2b6
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2015-10-30 21:27:55 +0900

    Initial Commit

---
+++ test/hello.spec.js
@@ -0,0 +1,8 @@
+var assert = require('power-assert');
+
+describe("karma and mocha testing", function() {
+  it ("return hello", function() {
+    assert(hello() !== "hello");
+  });
+});
+          
